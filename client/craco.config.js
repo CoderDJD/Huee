34e603062505c693d191372e5e71a27fab853405
Yomi adenaike <oluwayomi@192.168.1.254>
2021-04-16 13:58:21 +0100

    configurations

---
+++ client/craco.config.js
@@ -0,0 +1,21 @@
+// craco.config.js
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")]
+    }
+  },
+  plugins: [
+    {
+      plugin: require("craco-alias"),
+      options: {
+        source: "tsconfig",
+        baseUrl: "./src",
+        tsConfigPath: "./tsconfig.extend.json"
+      }
+    },
+    {
+      plugin: require("craco-plugin-scoped-css")
+    }
+  ]
+};
