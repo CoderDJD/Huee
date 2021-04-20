651033aef2278347c31c8996ec60f4cff743b7b0
Yomi adenaike <oluwayomi@192.168.1.254>
2021-04-20 10:16:41 +0100

    redux

---
+++ client/store/index.ts
@@ -0,0 +1,22 @@
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from "../../types/user";
+const usersReducer = createSlice({
+  name: "user",
+  initialState: {
+    data: {} as User
+  },
+  reducers: {
+    UPDATE_CURRENT_USER(state, action: PayloadAction<User>) {
+      state.data = action.payload;
+    }
+  }
+});
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer.reducer
+  }
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type RootDispatch = typeof store.dispatch;
