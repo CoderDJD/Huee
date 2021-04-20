a78a10a58133452b304a2443f33ffbe5934908bc
Yomi Adenaike <adenaikeyomi@gmail.com>
2021-04-20 15:22:50 +0100

    base

--- client/src/modules/auth/Auth.tsx
+++ client/src/modules/auth/Auth.tsx
@@ -1,14 +1,14 @@
 import React, { lazy } from "react";
 import { Route, RouteComponentProps, Switch } from "react-router";
 const SignIn = lazy(() => import("./sign_in/SignIn"));
-const SignUp = lazy(() => import("./sign_up/SignUp"));
+const GetStarted = lazy(() => import("./get_started/GetStarted"));
 
 const Auth = ({ match: { url } }: RouteComponentProps) => {
   return (
     <div>
       <Switch>
         <Route exact path={`${url}/signin`} component={SignIn} />
-        <Route exact path={`${url}/getstarted`} component={SignUp} />
+        <Route exact path={`${url}/getstarted`} component={GetStarted} />
         <Route />
       </Switch>
     </div>
